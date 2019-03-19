f6a373d6ddfa946894755ffcfc0ed2801e9c0250
doming <domingfan@163.com>
2019-03-19 15:18:43 +0800

    list 增加和插入元素

--- string/string.cpp
+++ string/string.cpp
@@ -1,7 +1,7 @@
 /* STL 
 
 list??
-??????
+???????
 */
 
 
@@ -15,16 +15,24 @@ list??
 
 int main()
 {
-   std::list<std::string> words;
-   std::list<std::string> sayings {20}; // A list of 20 empty strings
-   std::list<double> values(50, 3.1415926); //??????????? {50? 3.1415926} ????????
-   std::list<double> save_values {values};  //save_values ? values ????
-   std::list<double> sample_values {++values.cbegin(), --values.cend()};  //sample_values ?values????????49?????
-                                                                          //??std::list<double> samples {++cbegin(values), --cend(values)};???vscode?
+   std::list<std::string> words {"Jane", "Jim", "Jules", "Janet"};
+   words.push_front("Ian");     // Add string ("Ian") to the front of the list
+   words.push_back("Kitty");    // Append string ("Kitty") to the end of the list
+   //?????????????????????????????????? pushJfront() ? push_back() ?????????????
+   words.emplace_front("Ian");  //Create string ("Ian") in place at the front of the list   
+   words.emplace_back("Kitty"); // Create string ("Kitty") in place at the end of the list
 
-    for(const auto name : sample_values)
+   //?????????  insert() ????????????????????????????????
+   words.insert(++begin(words), "May"); // Insert "May" as the second element  
+
+   auto iter = begin(words);    //iter ? list<std::string>::iterator ??
+   std::advance(iter, 9); // Increase iter by 9  ??????9
+   words.insert(iter, 3, "Dmm");// Insert 3 copies of 88 starting at the 10th
+
+
+    for(const auto name : words)
     std::cout << name << std::endl;
-    printf("sample_values.size():%d",sample_values.size());
+    printf("sample_values.size():%d",words.size());
 
     printf("end!");
 
